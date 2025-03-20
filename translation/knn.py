6964a13897f45a87a0dee80a6a79321b2c33c4f8
Ken Sible <ksible@outlook.com>
2025-03-20 18:47:14 -0400

    removed spaCy lemmatizer from KNN and refactored

--- translation/knn.py
+++ translation/knn.py
@@ -1,66 +1,23 @@
 import pickle
 
 import numpy as np
-import numpy.typing as npt
 from scipy.spatial.distance import cosine
 from sklearn.neighbors import NearestNeighbors
 from tqdm import tqdm
 
-from translation.manager import Lemmatizer, Manager, Tokenizer
+from translation.manager import Manager, Tokenizer
 
 
 class KNNModel:
-    def __init__(self, manager: Manager, spacy_model: str, freq_file: str, *, n_neighbors: int):
+    def __init__(self, manager: Manager, freq_file: str, *, n_neighbors: int):
         self.manager = manager
-        self.n_neighbors = n_neighbors
-
-        self.freq = {}
+        self.tokenizer = Tokenizer(manager.src_lang, sw_model=manager.sw_model)
         with open(freq_file) as freq_f:
+            self.freq = {}
             for line in freq_f.readlines():
                 word, freq = line.split()
                 self.freq[word] = float(freq)
-        self.tokenizer = Tokenizer(manager.src_lang, sw_model=manager.sw_model)
-        self.lemmatizer = Lemmatizer(spacy_model, manager.sw_model)
-
-    def _metric(self, u: npt.NDArray, v: npt.NDArray, a: int = 1, b: int = 1) -> npt.NDArray:
-        # return np.dot(u[:-1], v[:-1])
-
-        # return minkowski(u[:-1], v[:-1])
-
-        # if u[-1:] == v[-1:]:
-        #     return a * minkowski(u[:-1], v[:-1])
-        # return a * minkowski(u[:-1], v[:-1]) + b / abs(u[-1:] - v[-1:])
-
-        return cosine(u[:-1], v[:-1])
-
-        # if u[-1:] == v[-1:]:
-        #     return a * cosine(u[:-1], v[:-1]) + b
-        # return a * cosine(u[:-1], v[:-1]) + b / abs(u[-1:] - v[-1:])
-
-    def fit(self):
-        model, vocab = self.manager.model, self.manager.vocab
-        self.nbrs = NearestNeighbors(
-            n_neighbors=self.n_neighbors, algorithm='ball_tree', metric=self._metric
-        )
-
-        embs = []
-        for word in tqdm(self.freq, bar_format='Fitting Model {desc}{percentage:3.0f}%|{bar:10}'):
-            subwords = self.tokenizer.tokenize(word)
-            A = model.out_embed(vocab.numberize(subwords)).detach().numpy()
-            # A[np.linalg.norm(A, axis=1).argmax()]
-            embs.append(np.append(np.mean(A, axis=0), self.freq[word]))
-        self.nbrs.fit(np.array(embs))
-
-    def kneighbors(self, word: str) -> list[str]:
-        model, vocab = self.manager.model, self.manager.vocab
-        lemma = next(self.lemmatizer.nlp.pipe([word]))[0].lemma_
-        subwords = self.tokenizer.tokenize(lemma)
-        A = model.out_embed(vocab.numberize(subwords)).detach().numpy()
-        # if lemma not in self.freq:
-        #     emb = np.append(np.mean(A, axis=0), 1e-9)
-        emb = np.append(np.mean(A, axis=0), self.freq.get(lemma, 0))
-        indices = self.nbrs.kneighbors(emb[None, ...], return_distance=False)
-        return [list(self.freq.keys())[index] for index in indices[0]]
+        self.n_neighbors = n_neighbors
 
     def save(self, model_file: str):
         with open(model_file, 'wb') as model_f:
@@ -69,3 +26,29 @@ class KNNModel:
     def load(self, model_file: str):
         with open(model_file, 'rb') as model_f:
             self.nbrs = pickle.load(model_f)
+
+    @staticmethod
+    def cosine_similarity(u: np.ndarray, v: np.ndarray) -> float:
+        return cosine(u[:-1], v[:-1])
+
+    def fit(self):
+        model, vocab = self.manager.model, self.manager.vocab
+        self.nbrs = NearestNeighbors(
+            n_neighbors=self.n_neighbors, algorithm='ball_tree', metric=self.cosine_similarity
+        )
+        word_embeds = []
+        for word in tqdm(self.freq, bar_format='Fitting Model {desc}{percentage:3.0f}%|{bar:10}'):
+            subwords = self.tokenizer.tokenize(word)
+            subword_embeds = model.out_embed(vocab.numberize(subwords)).detach().numpy()
+            word_embeds.append(np.append(np.mean(subword_embeds, axis=0), self.freq[word]))
+        self.nbrs.fit(np.array(word_embeds))
+
+    def kneighbors(self, word: str) -> list[str]:
+        model, vocab = self.manager.model, self.manager.vocab
+        tokens = self.tokenizer.tokenize(word)
+        subword_embeds = model.out_embed(vocab.numberize(tokens)).detach().numpy()
+        # if word not in self.freq:
+        #     word_embed = np.append(np.mean(A, axis=0), 1e-9)
+        word_embed = np.append(np.mean(subword_embeds, axis=0), self.freq.get(word, 0))
+        indices = self.nbrs.kneighbors(word_embed[None, ...], return_distance=False)
+        return [list(self.freq.keys())[index] for index in indices[0]]
