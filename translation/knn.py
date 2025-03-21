44d71d6f38d3191714558ecf110d5a439c73a3ed
Ken Sible <ksible@outlook.com>
2025-03-21 01:45:44 -0400

    added inverse frequency penalty to KNN metric

--- translation/knn.py
+++ translation/knn.py
@@ -17,6 +17,7 @@ class KNNModel:
             for line in freq_f.readlines():
                 word, freq = line.split()
                 self.freq[word] = float(freq)
+            self.vocab = list(self.freq.keys())
         self.n_neighbors = n_neighbors
 
     def save(self, model_file: str):
@@ -28,16 +29,20 @@ class KNNModel:
             self.nbrs = pickle.load(model_f)
 
     @staticmethod
-    def cosine_similarity(u: np.ndarray, v: np.ndarray) -> float:
-        return cosine(u[:-1], v[:-1])
+    def cosine_similarity(u: np.ndarray, v: np.ndarray, a: int, b: int) -> float:
+        # return cosine(u[:-1], v[:-1]) / (1 + np.log(1 + v[-1]))
+        return cosine(u[:-1], v[:-1]) + a / (v[-1] + b)
 
     def fit(self):
         model, vocab = self.manager.model, self.manager.vocab
         self.nbrs = NearestNeighbors(
-            n_neighbors=self.n_neighbors, algorithm='ball_tree', metric=self.cosine_similarity
+            n_neighbors=self.n_neighbors + 1,
+            algorithm='ball_tree',
+            metric=self.cosine_similarity,
+            metric_params={'a': 10.0, 'b': 1e-6},
         )
         word_embeds = []
-        for word in tqdm(self.freq, bar_format='Fitting Model {desc}{percentage:3.0f}%|{bar:10}'):
+        for word in tqdm(self.vocab, bar_format='Fitting Model {desc}{percentage:3.0f}%|{bar:10}'):
             subwords = self.tokenizer.tokenize(word)
             subword_embeds = model.out_embed(vocab.numberize(subwords)).detach().numpy()
             word_embeds.append(np.append(np.mean(subword_embeds, axis=0), self.freq[word]))
@@ -47,8 +52,8 @@ class KNNModel:
         model, vocab = self.manager.model, self.manager.vocab
         tokens = self.tokenizer.tokenize(word)
         subword_embeds = model.out_embed(vocab.numberize(tokens)).detach().numpy()
-        # if word not in self.freq:
-        #     word_embed = np.append(np.mean(A, axis=0), 1e-9)
-        word_embed = np.append(np.mean(subword_embeds, axis=0), self.freq.get(word, 0))
-        indices = self.nbrs.kneighbors(word_embed[None, ...], return_distance=False)
-        return [list(self.freq.keys())[index] for index in indices[0]]
+        word_embed = np.append(np.mean(subword_embeds, axis=0), self.freq.get(word, 0.0))
+        indices = self.nbrs.kneighbors(word_embed[None, ...], return_distance=False)[0]
+        return [self.vocab[index] for index in indices if self.vocab[index] != word][
+            : self.n_neighbors
+        ]
